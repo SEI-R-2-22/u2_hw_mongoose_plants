69b684f1db5433a673413f0913f11fc9568e53fb
Jenna Leopold <jleopold424@gmail.com>
2022-03-22 17:07:21 -0400

    add routes for creating plant, getting all plants, getting plant by id

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const { Router } = require('express')
+const controllers = require('../controllers')
 const router = Router()
 
 router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/plants', controllers.createPlant)
+
+router.get('/plants', controllers.getAllPlants)
+
+router.get('/plants/:id', controllers.getPlantById)
 
 module.exports = router
