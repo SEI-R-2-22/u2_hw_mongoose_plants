b50197d4c14bd37bfa960d0c470bfc934e8fb944
Jenna Leopold <jleopold424@gmail.com>
2022-03-22 17:02:35 -0400

    define getPlantById controller

--- controllers/index.js
+++ controllers/index.js
@@ -21,7 +21,21 @@ const getAllPlants = async (req, res) => {
   }
 }
 
+const getPlantById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const plant = await Plant.findById(id)
+    if (plant) {
+      return res.status(200).json({ plant })
+    }
+    return res.status(404).send('Plant with the specified ID does not exists')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
   createPlant,
-  getAllPlants
+  getAllPlants,
+  getPlantById
 }
