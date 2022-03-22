12ff734ef740a551fa424509fd23e5ca42226f45
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-03-22 18:52:08 -0400

    finished plants project

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const controllers = require('../controllers')
+const router = Router();
+
+router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/plants', controllers.createPlant)
+router.get('/plants', controllers.getAllPlants)
+router.get('/plants/:id', controllers.getPlantById)
+router.put('/plants/:id', controllers.updatePlant)
+router.delete('/plants/:id', controllers.deletePlant)
+
+module.exports = router;
+
+//"_id": "623a4f9b5e9433085cd52d82"
