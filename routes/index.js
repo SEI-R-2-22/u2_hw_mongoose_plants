5a2d6a06c7b1685e266b47684b4ede248a92226d
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-03-22 17:46:22 -0400

    removed some code that was breaking the update

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ router.get('/', (req, res) => res.send('This is root!'));
 router.post('/plants', controllers.createPlant);
 router.get('/plants', controllers.getAllPlants);
 router.get('/plants/:id', controllers.getPlantById);
-router.put('/plants/:id', controllers.updatePlant)
-router.delete('/plants/:id', controllers.deletePlant)
+router.put('/plants/:id', controllers.updatePlant);
+router.delete('/plants/:id', controllers.deletePlant);
 
 
 module.exports = router;
