43d0df363b96d0d48e4aaed295b2fd36e3af85ec
Jackson Christopher <kitchen.king.95@gmail.com>
2022-03-22 20:57:38 -0500

    plants

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', (req, res) => res.send('This is root!'))
+router.get('/plants', controllers.getAllPlants)
+router.get('/plants/:id', controllers.getPlantById)
+router.put('/plants/:id', controllers.updatePlant)
+router.delete('/plants/:id', controllers.deletePlant)
+
+module.exports = router;
