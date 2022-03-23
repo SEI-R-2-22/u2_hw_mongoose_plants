38bb4687ab67706fe48f57e4adb86c414ef142fb
unknown <sbandy9210@gmail.com>
2022-03-23 16:46:23 -0500

    finished

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,64 @@ const createPlant = async (req, res) => {
     }
 }
 
+const getAllPlants = async (request, response) => {
+    try{
+        const plants = await Plant.find()
+        return response.status(200).json({ plants })
+    }catch (error){
+        return response.status(500).send(error.message)
+}
+}
+
+
+
+const getPlantById = async (request, response) => {
+    try{ 
+        const { id } = request.params
+        const plant = await Plant.findById(id)
+        if (plant) {
+            return response.status(200).json({ plant })
+        }
+        return res.status(404).send('Plant with this ID not found')
+    }catch (error) {
+        return response.status(500).send(error.message)
+    }
+}
+
+const updatePlant = async (request, response) => {
+    try {
+        const { id } = request.params;
+        await Plant.findByIdAndUpdate(id, request.body, { new: true }, (err, plant) => {
+            if (err) {
+                response.status(500).send(err);
+            }
+            if (!plant) {
+                response.status(500).send('Plant not found!');
+            }
+            return response.status(200).json(plant);
+        })
+    } catch (error) {
+        return response.status(500).send(error.message);
+    }
+}
+
+const deletePlant = async (request, response) => {
+    try {
+        const { id } = request.params;
+        const deleted = await Plant.findByIdAndDelete(id)
+        if (deleted) {
+            return response.status(200).send("Plant deleted");
+        }
+        throw new Error("Plant not found");
+    } catch (error) {
+        return response.status(500).send(error.message);
+    }
+}
+
 module.exports = {
     createPlant,
+    getAllPlants,
+    getPlantById,
+    updatePlant,
+    deletePlant
 }
--- server.js
+++ server.js
@@ -2,19 +2,21 @@ const express = require('express');
 const routes = require('./routes');
 const db = require('./db');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 // require() imports and middleware here ^ ///////
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+app.use(bodyParser.json())
+app.use(logger('dev'))
 
 
 // app.use() middleware here ^ ///////////////////
 
 app.use('/api', routes);
-app.use(bodyParser.json())
+
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
